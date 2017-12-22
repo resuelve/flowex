152eb508ce8d1bb9fe19ae6aad00d2aca16266ff
alanraul <raulumb@gmail.com>
2017-12-22 12:42:36 -0600

    Integración de intents y query

--- test/flowex_test.exs
+++ test/flowex_test.exs
@@ -4,8 +4,12 @@ defmodule FlowexTest do
 
   import Mock
 
-  test "greets the world" do
+  @response %HTTPoison.Response{status_code: 200, body: "{\"status\":\"success\"}"}
 
+  test "request/3 helper for request to Dialogflow" do
+    with_mock HTTPoison, [request: fn(_method, url, _body, _headers, _options) -> {:ok, @response} end] do
+      assert Flowex.request(:get, "intents", "") == {:ok, %{"status" => "success"}}
+    end
   end
 
 end
